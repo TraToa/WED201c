cd37cf9bbe7b7f00d828c1d63332651424c15dc0
Tran Le Khanh Toan <trantoan.tratoa@gmail.com>
2025-09-28 22:52:50 +0700

    Update JS homework

--- JavaScript/Homework/js/gallery.js
+++ JavaScript/Homework/js/gallery.js
@@ -1,5 +1,7 @@
 /*Name this external file gallery.js*/
 
+var element = document.getElementById("image")
+
 function upDate(previewPic){
  /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
@@ -11,7 +13,6 @@ function upDate(previewPic){
 
     var alt = previewPic.alt
     var src = previewPic.src
-    var element = document.getElementById("image")
 
     element.innerHTML = alt
 
@@ -23,7 +24,7 @@ function upDate(previewPic){
     }
 }
 
-	function unDo(){
+function unDo(){
      /* In this function you should 
     1) Update the url for the background image of the div with the id = "image" 
     back to the orginal-image.  You can use the css code to see what that original URL was
@@ -31,5 +32,13 @@ function upDate(previewPic){
     2) Change the text  of the div with the id = "image" 
     back to the original text.  You can use the html code to see what that original text was
     */
-		
-	}
+
+    element.innerHTML = "Hover over an image below to display here."
+
+    let sheet = document.styleSheets[0];
+    for (let rule of sheet.cssRules) {
+        if (rule.selectorText === "#image") {
+            rule.style.backgroundImage = "url(" + ")"
+        }
+    }
+}
